85b06ddc6c7e7faad8287cc948e6e75d5d57791c
Khalil-Burns <khalilburns@gmail.com>
2025-09-04 20:37:14 -0400

    fixed fret alignment issues"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import PianoApp from "@/components/Piano";
+import FretlyGuitar from "@/components/Fretboard";
 
 export default function Home() {
   return (
     <main>
       <PianoApp />
+      <FretlyGuitar />
     </main>
   );
 }
